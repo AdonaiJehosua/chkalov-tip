4f6085a79921d80e2968fc392ce386eac057fd07
AdonaiJehosua <adonaijehosua@gmail.com>
2023-02-27 00:01:36 +0500

    change adress

--- src/appContext/contactsContext.js
+++ src/appContext/contactsContext.js
@@ -6,7 +6,7 @@ export const ContactsContext = createContext({
     phone: '+79673695787',
     visiblePhone: '+7 (967) 369-57-87',
     email: 'zakaz@chkalov-tip.ru',
-    adress: 'Первоуральск, Чкалова 18В',
+    adress: 'Свердловская обл., г. Первоуральск, Чкалова 18В',
     workHours: 'Пн-Пт, 09:00-17:00',
     toString: function () {
         return `${this.companyName}\nТелефон: ${this.visiblePhone}\nПочта: ${this.email}\nАдрес: ${this.adress}\nВремя работы: ${this.workHours}`
