5c79e89d86b45e3a11ce916451a4ca325e1556ff
AdonaiJehosua <adonaijehosua@gmail.com>
2023-02-27 03:01:11 +0500

    wtf

--- src/components/pagesComponents/ContactsPage.js
+++ src/components/pagesComponents/ContactsPage.js
@@ -1,14 +1,59 @@
 import { Box } from "@mui/material"
-import { Button, GeolocationControl, Map, YMaps } from "@pbe/react-yandex-maps"
+import { Button, GeolocationControl, Map, Placemark, RouteButton, YMaps, useYMaps } from "@pbe/react-yandex-maps"
+import { useEffect, useRef } from "react";
 
 const style = {
 
 }
 
+
+
 export function ContactsPage() {
+
+    // const CustomMap = () => {
+    //     const mapRef = useRef(null);
+    //     const ymaps = useYMaps(['Map', 'Placemark']);
+
+    //     console.log(ymaps)
+
+    //     useEffect(() => {
+    //         if (!ymaps || !mapRef.current) {
+    //           return;
+    //         }
+        
+    //         new ymaps.Map(mapRef.current, {
+    //           center: [55.76, 37.64],
+    //           zoom: 10,
+    //         });
+
+    //         const button = new ymaps.control.Button({
+    //             data: {
+    //                 // Зададим иконку для кнопки.
+    //                 image: 'images/button.jpg',
+    //                 // Текст на кнопке.
+    //                 content: 'Save',
+    //                 // Текст всплывающей подсказки.
+    //                 title: 'Click to save the route'
+    //             },
+    //             options: {
+    //                 // Зададим опции кнопки.
+    //                 selectOnClick: false,
+    //                 // У кнопки будет три состояния: иконка, текст и текст + иконка.
+    //            // Зададим три значения ширины кнопки для всех состояний.
+    //                 maxWidth: [30, 100, 150]
+    //             }
+    //    });
+    //    ymaps.control.Button(button, { float: 'right', floatIndex: 100 });
+    //       }, [ymaps]);
+
+
+
+    //     return <div ref={mapRef} style={{ width: "320px", height: "240px" }}></div>;
+    // };
+
+
     return (
         <YMaps>
-
             <Box sx={style}>
                 Контакты
                 <Map defaultState={{ center: [56.89439849, 59.94808121], zoom: 17 }}>
@@ -17,7 +62,10 @@ export function ContactsPage() {
                         data={{ content: "Unpress me!" }}
                         defaultState={{ selected: true }}
                     />
-                        <GeolocationControl options={{ float: "left" }} />
+                    <GeolocationControl options={{ float: "left" }} />
+                    <Placemark geometry={[56.89439849, 59.94808121]} />
+                    <RouteButton options={{ float: "right" }} defaultState={{ to: "moscow", fromEnabled: 'false' }} />
+
 
                 </Map>
             </Box>
--- src/pages/contacts/index.js
+++ src/pages/contacts/index.js
@@ -1,8 +1,11 @@
 import { ContactsPage } from "@/components/pagesComponents/ContactsPage";
 import PageWithHead from "@/components/reusableComponents/PageWithHead";
+import { YMaps } from "@pbe/react-yandex-maps";
 
 export default function Contacts() {
     return (
+        <YMaps>
         <PageWithHead title={'Контакты'} pageComponent={<ContactsPage/>}/>
+        </YMaps>
     )
 }
