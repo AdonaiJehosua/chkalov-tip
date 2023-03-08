38eff215820ae5770e6d4a7e9158893be22cd95f
AdonaiJehosua <adonaijehosua@gmail.com>
2023-03-09 03:08:58 +0500

    add space

--- src/components/fone/house.js
+++ src/components/fone/house.js
@@ -2,6 +2,7 @@ import { Box } from "@mui/material"
 import { mainLayoutComponentStyles } from "@/styles/mainLayoutComponentStyles" 
 
 export const House = () => {
+
     return (
         <Box sx={mainLayoutComponentStyles.house} />
     )
