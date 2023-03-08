de283354d2f8873cf224beece7238c982cf96b2e
AdonaiJehosua <adonaijehosua@gmail.com>
2023-03-09 03:12:18 +0500

    some style changes

--- src/components/pagesComponents/ContactsPage.js
+++ src/components/pagesComponents/ContactsPage.js
@@ -1,23 +1,35 @@
 import { Box } from "@mui/material"
 import { Button, GeolocationControl, Map, Placemark, RouteButton, YMaps } from "@pbe/react-yandex-maps"
-
-const style = {
-    wrapper: {
-        width: '100%',
-        height: '100%'
-    },
-    mapWrapper: {
-        width: '50%',
-        height: '80%',
-        border: '1px solid black',
-        borderRadius: '10px',
-        overflow: 'hidden'
-    }
-}
-
-
+import { useState } from "react"
 
 export function ContactsPage() {
+
+    const [mapContainerSize, setMapContainerSize] = useState(false)
+
+    const mapSizeHandle = () => {
+        setMapContainerSize((prevState) => !prevState)
+    }
+
+    const style = {
+        wrapper: {
+            width: '100%',
+            height: '100%'
+        },
+        mapWrapper: {
+            width: mapContainerSize ? '20%' : '100%',
+            height: mapContainerSize ? '10%' : '80%',
+            border: '1px solid black',
+            borderRadius: '10px',
+            overflow: 'hidden'
+        },
+        mapSizeButton: {
+            maxWidth: '128px'
+        },
+        map: {
+            width: '100%',
+            height: '100%'
+        }
+    }
 
     return (
         <Box sx={style.wrapper}>
@@ -25,10 +37,11 @@ export function ContactsPage() {
             <YMaps query={{
                 apikey: 'aa28440e-21cf-421f-8f0e-a5ee25084a75'
             }}>
-                <Map defaultState={{ center: [56.89439849, 59.94808121], zoom: 17 }} modules={['geolocation', 'geocode']} width={'100%'} height={'100%'}>
+                <Map defaultState={{ center: [56.89439849, 59.94808121], zoom: 17 }} modules={['geolocation', 'geocode']} style={style.map}>
                     <Button
-                        options={{ maxWidth: 128 }}
-                        data={{ content: "Unpress me!" }}
+                        onClick={mapSizeHandle}
+                        options={style.mapSizeButton}
+                        data={{ content: mapContainerSize ? 'Развернуть карту' : 'Свернуть карту'}}
                         defaultState={{ selected: true }}
                     />
                     <GeolocationControl options={{ float: "left" }} />
