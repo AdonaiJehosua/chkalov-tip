00e89b9c3cc8ff09e6e36d28b45441d9141f88ac
AdonaiJehosua <adonaijehosua@gmail.com>
2023-03-09 03:10:01 +0500

    add space

--- src/components/fone/animatedElems/foneCloud.js
+++ src/components/fone/animatedElems/foneCloud.js
@@ -3,7 +3,8 @@ import foneCloud from '../../../media/images/foneCloud.svg'
 import { mainLayoutComponentStyles } from "@/styles/mainLayoutComponentStyles" 
 
 export const FoneCloud = () => {
+
     return (
-        <Box sx={mainLayoutComponentStyles.foneCloud} component='img' src={foneCloud.src}/>
+        <Box id={'foneCloud'} sx={mainLayoutComponentStyles.foneCloud} component='img' src={foneCloud.src}/>
     )
 }
